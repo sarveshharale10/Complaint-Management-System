a8489498847c20222a5c662e3d64d09130aaec0d
sarvesh <sarvesh@gmail.com>
2019-04-14 18:22:43 +0530

    Final Twitching

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
+var mysql = require('mysql');
 var express = require('express');
 var app = express();
-var nunjucks = require('nunjucks');
+var session = require('express-session');
 var bodyParser = require('body-parser');
-var router = express.Router();
+var nunjucks = require('nunjucks');
+var flash = require("express-flash");
+var path = require("path");
 
-var mysql = require('mysql');
-var conn = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"sarvesh",
-    database:"cms"
+var connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : 'sarvesh',
+	database : 'cms'
 });
 
 nunjucks.configure('views', {
@@ -17,53 +19,136 @@ nunjucks.configure('views', {
     express: app
 });
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(flash());
 
-app.get("/form",function(req,res){
-    res.render("form.njk");
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+
+app.use("/public",express.static(path.join(__dirname,"public")));
+
+app.get('/', function(request, response) {
+	response.render("login.njk",{req:request});
 });
 
-/*app.post("/login",function(req,res){
-    res.render("first_template.njk",{name:req.body.username});
-});*/
-
-app.use("/complaint",router);
-
-router.get("/",function(req,res){
-    conn.connect(function(err){
-        if(err) throw err;
-        conn.query("select * from complaint",function(err,result,fields){
-            if(err) throw err;
-            res.send(result);
-        });
-    });
-});
-
-router.post('/auth',function(request,response){
-    var username = request.body.username;
+app.post('/auth', function(request, response) {
+	var username = request.body.username;
 	var password = request.body.password;
 	var query = "select * from user where username='"+username+"' and password='"+password+"'";
-	conn.connect(function(error){
-		if(error) throw error;
-		conn.query(query, function(err, result, fields){
-            if(err) throw err;
-            response.send(result);
+	connection.connect(function(error){
+		connection.query(query, function(err, result, fields){
+			if(err) throw err;
+			if (result.length > 0) {
+				request.session.loggedin = true;
+				request.session.username = username;
+				var userType = result[0].type;
+				if(userType == 0){
+					response.redirect("/admin");
+				}
+				else if(userType == 1){
+					response.redirect("/assigned");
+				}
+				else if(userType == 2){
+					response.redirect("/complaint");
+				}
+			} else {
+				request.flash("invalid","Invalid Username and/or Password");
+				response.redirect("/");
+			}
 		});
 	});
 });
 
-router.put("/",function(req,res){
-
-    conn.connect(function(err){
-        if(err) throw err;
-        var body = req.body;
-        var query = "insert into complaint values("+body.id+",'"+body.description+"','"+body.dateFiled+"',0)";
-        conn.query(query,function(err,result){
-            if(err) throw err;
-            res.json({status:1});
-        });
-    });
+app.get("/complaint",function(req,res){
+	if(req.session.username){
+		res.render("complaintform.njk",{username:req.session.username});
+	}
+	else{
+		res.redirect("/");
+	}
 });
 
+app.post("/complaint",function(req,res){
+	var email = req.body.email;
+	var contact = req.body.contact;
+	var desc = req.body.desc;
+	var query = "insert into complaint(username,email,contact,description,status) values('"+req.session.username+"','"+email+"','"+contact+"','"+desc+"',"+0+")";
+	connection.connect(function(error){
+		connection.query(query,function(err,result){
+			if(err) throw err;
+			req.flash("successful","Complaint Registered Successfully");
+			res.redirect("/complaint");
+		});
+	});
+});
+
+app.get("/assigned",function(req,res){
+	var query = "select complaint.complaintId,complaint.description from complaint,complaintAssignment where complaint.complaintId=complaintAssignment.id and complaint.status=0 and assigned='"+req.session.username+"'";
+	connection.connect(function(error){
+		connection.query(query,function(err,result){
+			if(err) throw err;
+			res.render("table.njk",{rows:result});
+		});
+	});
+});
+
+app.post("/assigned",function(req,res){
+	var complaintId = req.body.complaintId;
+	var query = "delete from complaintAssignment where id="+complaintId+" and assigned='"+req.session.username+"'";
+	connection.connect(function(error){
+		connection.query(query,function(err,result){
+			if(err) throw err;
+		});
+		query = "update complaint set status=1 where complaintId="+complaintId;
+		connection.query(query,function(err,result){
+			if(err) throw err;
+			res.redirect("/assigned");
+		});
+	});
+});
+
+app.get("/admin",function(req,res){
+	if(req.session.username){
+		var engineersQuery = "select username from user where type=1";
+
+		var query = "select complaintId,username,description from complaint where complaintId not in(select id from complaintAssignment) and status=0";
+		connection.connect(function(error){
+			connection.query(query,function(err,complaints){
+				if(err) throw err;
+				connection.query(engineersQuery,function(err,engineers,fields){
+					if(err) throw err;
+					res.render("admin.njk",{rows:complaints,engineers:engineers});
+				});
+			});
+		});
+	}
+	else{
+		res.redirect("/");
+	}
+});
+
+app.post("/assign",function(req,res){
+	var complaintId = req.body.complaintId;
+	var enggId = req.body.enggId;
+	var query = "insert into complaintAssignment values("+complaintId+",'"+enggId+"')";
+	connection.connect(function(error){
+		connection.query(query,function(err,result){
+			if(err) throw err;
+			req.flash("successful","Complaint Assigned Successfully");
+			res.redirect("/admin");
+		});
+	});
+});
+
+app.get("/logout",function(req,res){
+	if(req.session){
+		req.session.destroy(function(err){
+			res.redirect("/")
+		});
+	}
+});
 
 app.listen(80);
